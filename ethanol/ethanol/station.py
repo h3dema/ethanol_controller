57fc1753e3893901b8f660768fcd7816693a18a1
Matheus Nunes <mhnnunes@dcc.ufmg.br>
2017-10-12 17:18:30 -0300

    Format ethanol code in PEP-8 standards

--- ethanol/ethanol/station.py
+++ ethanol/ethanol/station.py
@@ -37,7 +37,8 @@ list_of_stations = {}
 def add_station(client_address):
     '''
       Create (and return) possibly several objects,
-      one for each wireless connections identified by (client_address, interface name).
+      one for each wireless connections identified by (client_address,
+      interface name).
       This function updates a list of these objects.
 
       client_address = (ip, port) used by the Hello message's process
@@ -49,7 +50,8 @@ def add_station(client_address):
         msg, intfs = get_interfaces(server=client_address, m_id=0)
         ''' select only wireless interfaces '''
         intfs = [intf.intf_name for intf in intfs if intf.is_wifi is True]
-        log.info("Found %d wireless interface in the device: %s", len(intfs), ",".join(intfs))
+        log.info("Found %d wireless interface in the device: %s", len(intfs),
+                 ",".join(intfs))
         if len(intfs) > 0:
             list_of_stations[ip] = {}
             for intf_name in intfs:
@@ -79,7 +81,8 @@ def get_station_by_ip(ip):
 
 def is_sta_with_ip_connected(ip):
     """
-      @note: this is the ip of the STA's interface that sends packets to the controller, i.e., normally it is an ethernet interface
+      @note: this is the ip of the STA's interface that sends packets to the
+      controller, i.e., normally it is an ethernet interface
       @return: TRUE if an STA with the ip provided as a parameter is connected
     """
     return ip in list_of_stations
@@ -93,101 +96,106 @@ class Station(Device):
     '''
 
     def __init__(self, socket, intf_name='wlan0'):
-      ''' constructor:
+        ''' constructor:
             creates an object that represents the user connection
             receives an ip/port pair from the hello message
             uses this info to connect to the station
             and retrieve the radio it is connected to
-      '''
-      log.info('constructor Station (%s,%s)' % socket)
-      super(Station, self).__init__(socket, intf_name)
+        '''
+        log.info('constructor Station (%s,%s)' % socket)
+        super(Station, self).__init__(socket, intf_name)
 
-      msg, mac_addr, ssid, freq, intf = get_sta_link_info(socket,id = self.msg_id,intf_name=intf_name)
-      log.info("get_sta_link_info - mac:%s ssid:%s freq:%d intf:%s" % (mac_addr, ssid, freq, intf))
-      self.__mac_address = mac_addr
+        msg, mac_addr, ssid, freq, intf = \
+            get_sta_link_info(socket, id=self.msg_id, intf_name=intf_name)
+        log.info("get_sta_link_info - mac:%s ssid:%s freq:%d intf:%s"
+                 % (mac_addr, ssid, freq, intf))
+        self.__mac_address = mac_addr
 
-      self.__linkando()
-      log.info('Station created')
+        self.__linkando()
+        log.info('Station created')
 
     def __linkando(self):
-      self.__vap = get_vap_by_mac_address(self.__mac_address)
-      if self.__vap == None:
-        self.__radio = None
-        log.debug("VAP <<nao encontrada>> na criacao da Station")
-      else:
-        self.__radio = self.__vap.radio
-        self.__vap.register_station(station=self)
-        log.debug("VAP <<encontrada>> na criacao da Station")
-
+        self.__vap = get_vap_by_mac_address(self.__mac_address)
+        if self.__vap is None:
+            self.__radio = None
+            log.debug("VAP <<nao encontrada>> na criacao da Station")
+        else:
+            self.__radio = self.__vap.radio
+            self.__vap.register_station(station=self)
+            log.debug("VAP <<encontrada>> na criacao da Station")
 
     def __del__(self):
-      ''' destructor '''
-      self.__vap.unregister_station(self)
+        ''' destructor '''
+        self.__vap.unregister_station(self)
 
-      if self.__ip in list_of_stations and self.intf_name in list_of_stations[self.__id]:
-        del list_of_stations[self.__id][self.intf_name]
+        if self.__ip in list_of_stations and \
+                self.intf_name in list_of_stations[self.__id]:
+            del list_of_stations[self.__id][self.intf_name]
 
     @property
     def vap(self):
-      ''' the VAP the station is connected to
-      '''
-      return self.__vap
+        ''' the VAP the station is connected to
+        '''
+        return self.__vap
 
     @property
     def radio(self):
-      ''' this station is connected to radio,
+        ''' this station is connected to radio,
           if radio == None the AP is not ethanol enabled
-      '''
-      return self.__radio
+        '''
+        return self.__radio
 
     @property
     def wireless_interfaces(self):
-      ''' returns all wireless enabled interfaces of the device
-      '''
-      server = self.get_connection
-      msg, intfs = get_interfaces(server,id = 0)
-      intfs = [intf.intf_name for intf in intfs if intf.is_wifi == True]
-      return intfs
+        ''' returns all wireless enabled interfaces of the device
+        '''
+        server = self.get_connection
+        msg, intfs = get_interfaces(server, id=0)
+        intfs = [intf.intf_name for intf in intfs if intf.is_wifi is True]
+        return intfs
 
     def getInterferenceMap(self):
-      ''' not implemented yet '''
-      pass
+        ''' not implemented yet '''
+        pass
 
     def getChannelInfo(self):
-      ''' not implemented yet '''
-      pass
+        ''' not implemented yet '''
+        pass
 
     def getBeaconInfo(self):
-      ''' not implemented yet '''
-      pass
+        ''' not implemented yet '''
+        pass
 
     def getNoiseInfo(self):
-      ''' not implemented yet '''
-      pass
+        ''' not implemented yet '''
+        pass
 
     def getLinkMeasurement(self):
-      ''' not implemented yet '''
-      pass
+        ''' not implemented yet '''
+        pass
 
     def getStatistics(self):
-      ''' not implemented yet '''
-      pass
+        ''' not implemented yet '''
+        pass
 
     def getLocation(self):
-      ''' not implemented yet '''
-      pass
+        ''' not implemented yet '''
+        pass
 
     def triggerTransition(self, new_vap):
-      '''uses message MSG_TRIGGER_TRANSITION to send to the station a command to change to a new ap
-
-      @param new_ap: MAC address of the new AP
-      '''
-      server = self.get_connection
-      station_trigger_transition(server, self.msg_id, sta_mac=self.mac_address, intf_name=self.intf_name, mac_new_ap=new_vap)
+        '''uses message MSG_TRIGGER_TRANSITION to send to the station a command
+        to change to a new ap
+        @param new_ap: MAC address of the new AP
+        '''
+        server = self.get_connection
+        station_trigger_transition(server, self.msg_id,
+                                   sta_mac=self.mac_address,
+                                   intf_name=self.intf_name,
+                                   mac_new_ap=new_vap)
 
     def __str__(self):
-      ''' string representation of this station
-      '''
-      ip, port = self.get_connection
-      return "Station %s %s conn(%s:%d)" % (self.intf_name, self.__mac_address, ip, port)
-
+        ''' string representation of this station
+        '''
+        ip, port = self.get_connection
+        return "Station %s %s conn(%s:%d)" % (self.intf_name,
+                                              self.__mac_address, ip, port)
--- ethanol/ethanol/switch.py
+++ ethanol/ethanol/switch.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 #
 # ##################################
-#  
+#
 # Copyright 2015 Henrique Moura
 #
 # This file is part of Ethanol.
-# 
+#
 # ##################################
-# 
+#
 
 '''  An L2 learning switch '''
 
@@ -24,131 +24,137 @@ log = core.getLogger()
 # Can be overriden on commandline.
 _flood_delay = 0
 
+
 class LearningSwitch(object):
 
-  def __init__(self, connection, transparent, idle_timeout = 10, hard_timeout = 30):
-    # We just use this to know when to log a helpful message
-    self.hold_down_expired = _flood_delay == 0
-    # Switch we'll be adding L2 learning switch capabilities to
-    self.connection = connection
-    self.transparent = transparent
-    self.__idle_timeout = idle_timeout
-    self.__hard_timeout = hard_timeout
+    def __init__(self, connection, transparent, idle_timeout=10,
+                 hard_timeout=30):
+        # We just use this to know when to log a helpful message
+        self.hold_down_expired = _flood_delay == 0
+        # Switch we'll be adding L2 learning switch capabilities to
+        self.connection = connection
+        self.transparent = transparent
+        self.__idle_timeout = idle_timeout
+        self.__hard_timeout = hard_timeout
 
-    # Our table
-    self.macToPort = {}
+        # Our table
+        self.macToPort = {}
 
-    # We want to hear PacketIn messages, so we listen to the connection
-    connection.addListeners(self)
+        # We want to hear PacketIn messages, so we listen to the connection
+        connection.addListeners(self)
 
-  def __flood(self, message = None):
-    """ Floods the packet """
-    msg = of.ofp_packet_out()
-    if time.time() - self.connection.connect_time >= _flood_delay:
-      # Only flood if we've been connected for a little while...
+    def __flood(self, message=None):
+        """ Floods the packet """
+        msg = of.ofp_packet_out()
+        if time.time() - self.connection.connect_time >= _flood_delay:
+            # Only flood if we've been connected for a little while...
 
-      if self.hold_down_expired is False:
-        # Oh yes it is!
-        self.hold_down_expired = True
-        log.info("%s: Flood hold-down expired -- flooding",
-            dpid_to_str(self.__dpid))
+            if self.hold_down_expired is False:
+                # Oh yes it is!
+                self.hold_down_expired = True
+                log.info("%s: Flood hold-down expired -- flooding",
+                         dpid_to_str(self.__dpid))
 
-      if message is not None: log.debug(message)
-      #log.debug("%i: flood %s -> %s", event.dpid,packet.src,packet.dst)
-      
-      # OFPP_FLOOD is optional; on some switches you may need to change this to OFPP_ALL.
-      msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
-    else:
-      pass
-      #log.info("Holding down flood for %s", dpid_to_str(event.dpid))
-    msg.data = self.__data
-    msg.in_port = self.__in_port
-    self.connection.send(msg)
+            if message is not None:
+                log.debug(message)
+            # log.debug("%i: flood %s -> %s", event.dpid,packet.src,packet.dst)
 
-  """
+            # OFPP_FLOOD is optional;
+            # on some switches you may need to change this to OFPP_ALL.
+            msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        else:
+            pass
+            # log.info("Holding down flood for %s", dpid_to_str(event.dpid))
+        msg.data = self.__data
+        msg.in_port = self.__in_port
+        self.connection.send(msg)
+
+    """
     Drops this packet and optionally installs a flow to continue
     dropping similar ones for a while
-  """
-  def __drop(self, duration = None):
-    if duration is not None:
-      if not isinstance(duration, tuple):
-        duration =(duration,duration)
-      msg = of.ofp_flow_mod()
-      msg.match = of.ofp_match.from_packet(self.__packet)
-      msg.idle_timeout = duration[0]
-      msg.hard_timeout = duration[1]
-      msg.buffer_id = self.__data.buffer_id
-      self.connection.send(msg)
-    elif event.ofp.buffer_id is not None:
-      msg = of.ofp_packet_out()
-      msg.buffer_id = self.__data.buffer_id
-      msg.in_port = self.__in_port
-      self.connection.send(msg)
-
-  def _handle_PacketIn(self, event):
     """
-    Handle packet in messages from the switch to implement above algorithm.
-    """
-    self.__dpid = event.dpid
-    self.__packet = event.parsed
-    self.__data = event.ofp
-    self.__in_port = event.port
+    def __drop(self, duration=None):
+        if duration is not None:
+            if not isinstance(duration, tuple):
+                duration = (duration, duration)
+            msg = of.ofp_flow_mod()
+            msg.match = of.ofp_match.from_packet(self.__packet)
+            msg.idle_timeout = duration[0]
+            msg.hard_timeout = duration[1]
+            msg.buffer_id = self.__data.buffer_id
+            self.connection.send(msg)
+        elif event.ofp.buffer_id is not None:
+            msg = of.ofp_packet_out()
+            msg.buffer_id = self.__data.buffer_id
+            msg.in_port = self.__in_port
+            self.connection.send(msg)
 
-    self.macToPort[self.__packet.src] = self.__in_port # 1
+    def _handle_PacketIn(self, event):
+        """
+        Handle packet in messages from the switch to implement above algorithm.
+        """
+        self.__dpid = event.dpid
+        self.__packet = event.parsed
+        self.__data = event.ofp
+        self.__in_port = event.port
 
-    if not self.transparent: # 2
-      if self.__packet.type == self.__packet.LLDP_TYPE or self.__packet.dst.isBridgeFiltered():
-        __drop() # 2a
-        return
+        self.macToPort[self.__packet.src] = self.__in_port  # 1
 
-    if self.__packet.dst.is_multicast:
-      __flood() # 3a
-    else:
-      dst = self.__packet.dst
-      if dst not in self.macToPort: # 4
-        __flood("Port for %s unknown -- flooding" %(dst,)) # 4a
-      else:
-        port = self.macToPort[dst]
-        if port == self.__in_port: # 5
-          # 5a
-          log.warning("Same port for packet from %s -> %s on %s.%s.  Drop."
-              %(packet.src, dst, dpid_to_str(self.__dpid), port))
-          __drop(10)
-          return
-        # 6
-        log.debug("installing flow for %s.%i -> %s.%i" %
-                 (packet.src, event.port, packet.dst, port))
-        msg = of.ofp_flow_mod()
-        msg.match = of.ofp_match.from_packet(packet, event.port)
-        msg.idle_timeout = self.__idle_timeout
-        msg.hard_timeout = self.__hard_timeout
-        msg.actions.append(of.ofp_action_output(port = port))
-        msg.data = event.ofp # 6a
-        self.connection.send(msg)
+        if not self.transparent:  # 2
+            if self.__packet.type == self.__packet.LLDP_TYPE or \
+                    self.__packet.dst.isBridgeFiltered():
+                __drop()  # 2a
+                return
+
+        if self.__packet.dst.is_multicast:
+            __flood()  # 3a
+        else:
+            dst = self.__packet.dst
+            if dst not in self.macToPort:  # 4
+              __flood("Port for %s unknown -- flooding" %(dst,)) # 4a
+            else:
+                port = self.macToPort[dst]
+                if port == self.__in_port:  # 5
+                    # 5a
+                    log.warning("Same port for packet from %s -> %s on %s.%s.\
+                      Drop." %(packet.src, dst, dpid_to_str(self.__dpid),
+                                port))
+                    __drop(10)
+                    return
+                # 6
+                log.debug("installing flow for %s.%i -> %s.%i" %
+                         (packet.src, event.port, packet.dst, port))
+                msg = of.ofp_flow_mod()
+                msg.match = of.ofp_match.from_packet(packet, event.port)
+                msg.idle_timeout = self.__idle_timeout
+                msg.hard_timeout = self.__hard_timeout
+                msg.actions.append(of.ofp_action_output(port=port))
+                msg.data = event.ofp  # 6a
+                self.connection.send(msg)
 
 
 class l2_learning(object):
-  """
-  Waits for OpenFlow switches to connect and makes them learning switches.
-  """
-  def __init__(self, transparent):
-    core.openflow.addListeners(self)
-    self.transparent = transparent
+    """
+    Waits for OpenFlow switches to connect and makes them learning switches.
+    """
+    def __init__(self, transparent):
+        core.openflow.addListeners(self)
+        self.transparent = transparent
 
-  def _handle_ConnectionUp(self, event):
-    log.debug("Connection %s" %(event.connection,))
-    LearningSwitch(event.connection, self.transparent)
+    def _handle_ConnectionUp(self, event):
+        log.debug("Connection %s" %(event.connection,))
+        LearningSwitch(event.connection, self.transparent)
 
 
 def launch(transparent=False, hold_down=_flood_delay):
-  """
-  Starts an L2 learning switch.
-  """
-  try:
-    global _flood_delay
-    _flood_delay = int(str(hold_down), 10)
-    assert _flood_delay >= 0
-  except:
-    raise RuntimeError("Expected hold-down to be a number")
+    """
+    Starts an L2 learning switch.
+    """
+    try:
+        global _flood_delay
+        _flood_delay = int(str(hold_down), 10)
+        assert _flood_delay >= 0
+    except Exception:
+        raise RuntimeError("Expected hold-down to be a number")
 
-  core.registerNew(l2_learning, str_to_bool(transparent))
+    core.registerNew(l2_learning, str_to_bool(transparent))
