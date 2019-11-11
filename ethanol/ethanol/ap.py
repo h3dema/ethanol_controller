470c85e86e5c1a5ec6609ce9a368c75d4cc14065
genilson <genilsonisrael@gmail.com>
2019-11-11 20:28:21 -0300

    add_network function used outside of class Network to add the network to the set if it doesn't exist. Return of the function used in Debug to inform that the ssid already exists.

--- ethanol/ethanol/ap.py
+++ ethanol/ethanol/ap.py
@@ -176,7 +176,7 @@ class AP(object):
         """
         # import placed here to avoid 'import loop'
         from pox.ethanol.ethanol.radio import Radio
-        from pox.ethanol.ethanol.network import Network
+        from pox.ethanol.ethanol.network import Network, add_network
 
         self.__id = uuid.uuid4()  #
         # client_address tuple
@@ -221,7 +221,10 @@ class AP(object):
                     log.info("Detected a invalid SSID!!!")
                 else:
                     net = Network(ssid.ssid)
-                    log.info('[%s] added to network (SSID) list', ssid.ssid)
+                    if add_network(ssid.ssid, net):
+                        log.info('[%s] added to network (SSID) list', ssid.ssid)
+                    else:
+                        log.debug('Network SSID %s already exists', ssid.ssid)
             log.info('Creating and association the VAP objects')
             #
             # retrieve configured vaps
